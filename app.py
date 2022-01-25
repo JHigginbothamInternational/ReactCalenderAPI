d9fdb338ea728a37bea039f43b1a3c8357f556b1
jacobhigginbotham <j.higginbotham.international@gmail.com>
2022-01-24 22:38:10 -0600

    Added MonthSchema & ReminderSchema

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://gqejerzwvxploc:4fb8b6ecc338728e97443d0e3439e1adadff84951c95692cb0500b01813efc9a@ec2-3-216-113-109.compute-1.amazonaws.com:5432/da0d05h0s7grat"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://gqejerzwvxploc:4fb8b6ecc338728e97443d0e3439e1adadff84951c95692cb0500b01813efc9a@ec2-3-216-113-109.compute-1.amazonaws.com:5432/da0d05h0s7grat"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -35,6 +35,23 @@ class Reminder(db.Model):
         self.month = month
         self.year = year
         self.text = text
+
+class MonthSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "year", "days_in_month", "days_in_previous_month", "start_day")
+
+month_schema = MonthSchema()
+multiple_month_schema = MonthSchema(many=True)
+
+class ReminderSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "day", "month", "year", "text")
+
+reminder_schema = ReminderSchema()
+multiple_reminder_schema = ReminderSchema(many=True)
+
+month_schema = MonthSchema()
+multiple_month_schema = MonthSchema(many=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
